1143c0c938b0bac490413e67e2d5507a69b719d4
juanpvh <juancm_pvh@hotmail.com>
2018-06-23 13:58:00 -0300

    Delete install_easyengine.sh

--- install_easyengine.sh
+++
@@ -1,9 +0,0 @@
-#---------------------------------------------------------------------
-# Função: InstallBasics
-#    Instalçando EasyEngine
-#---------------------------------------------------------------------
-InstallBasics() {
-   echo -n "Instalando EasyEngine... "
-  wget -qO ee rt.cx/ee && sudo bash ee
-  echo -e "[${green}DONE${NC}]\n"
-}
