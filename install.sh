3dd8c2037e42b35bf6b0394b279752bebc9d3248
juanpvh <juancm_pvh@hotmail.com>
2018-08-12 17:23:54 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!bin/sh
+
+wget https://raw.githubusercontent.com/juanpvh/teste/master/menu.sh
+wget https://raw.githubusercontent.com/juanpvh/teste/master/menu02.sh
+wget https://raw.githubusercontent.com/juanpvh/teste/master/wp.sh
