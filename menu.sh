14417e11cd6a1e3dfb425ed9493edbadd92bce53
juanpvh <juancm_pvh@hotmail.com>
2018-08-12 17:17:55 -0300

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+red='\033[0;31m'
+green='\033[0;32m'
+NC='\033[0m' # No Color
+source /root/menu02.sh
+###MENU PRINCIPAL###
+if item=$(whiptail --title "Painel de Gerenciamento de Servidor" --menu "Escolha uma opção na lista abaixo" 0 0 0 \
+"1" "Instaladores" \
+"2" "Painel Easyengine" \
+"3" "Segurança" \
+"4" "Restart mysql,nginx,PHP..." \
+"5" "Monitoramento" \
+"Esc" "SAIR" 3>&1 1>&2 2>&3)
+then
+
+###INSTALADORES "item 1"###
+if [ $item -eq 1 ]; then
+./menu02.sh
+fi
+
+
+#######
+
+fi
+	
