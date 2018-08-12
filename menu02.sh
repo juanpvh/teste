75d604a6d071580cf952ba5d217ecb76bfe5aa18
juanpvh <juancm_pvh@hotmail.com>
2018-08-12 18:18:56 -0300

    Update menu02.sh

--- menu02.sh
+++ menu02.sh
@@ -1,35 +1,40 @@
 #! /bin/bash
-source /root/wp
-funinstall() {
-instaladores=$(whiptail --title "Instaladores" --menu " Escolha uma das opções para configurar sua VPS/Dedicado." 0 0 0 \
+
+instaladores=$(whiptail --title "Menu Instaladores" --menu " Escolha uma das opções para configurar sua VPS/Dedicado." 0 0 0 \
 "WP" "Instalar e Configurar VPS para Wordpress" \
 "MC" "Instalar e Configurar VPS para Mautic" \
 "CP" "Instalar e Configurar VPS para CyberPanel" \
 "PL" "Instalar e Configurar VPS para Postal" \
 "IC" "Instalar e Configurar VPS para IspConfig" \
-"Voltar" "Voltar para o Menu Principal" 3>&1 1>&2 2>&3)
+"6" "Voltar para o Menu Principal" 3>&1 1>&2 2>&3)
 
 ###FINAL DO INSTALADORES "ITEM 1"###
 ##EXECUÇÃO DOS INSTALADORES###
 ###Instalar e Configurar VPS para Wordpress###
+status=$?
 case $instaladores in
 WP)
-wp
+wp.sh
 ;;
-2)
+MC)
 vps-mc.sh
 ;;
-3)
+CP)
 vps-cp.sh
 ;;
-4)
+PL)
 vps-pl.sh
 ;;
-5)
+IC)
 vps-ic.sh
 ;;
 6)
-bash menu01.sh
+status=$?
+if [ $status = 0 ]; then
+menu.sh
+fi
+if [ $status = 1 ]; then
+menu02.sh
+fi
 ;;
 esac
-}
