664699c2177c40b09bcbdffc995b0ae9d1b95948
juanpvh <juancm_pvh@hotmail.com>
2018-08-12 17:24:51 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!bin/sh
+[ -f /bin/whiptail ] && echo -e "whiptail found: ${green}OK${NC}\n"  || apt-get -y install whiptail > /dev/null 2>&1
 
 wget https://raw.githubusercontent.com/juanpvh/teste/master/menu.sh
 wget https://raw.githubusercontent.com/juanpvh/teste/master/menu02.sh
 wget https://raw.githubusercontent.com/juanpvh/teste/master/wp.sh
+clear
